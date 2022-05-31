142d54825ff9296b17bd875f82cf4e5e89d15554
YeaminRabbi <yeaminrabbi308@gmail.com>
2022-06-01 02:53:07 +0600

    Project Setup | Appointment

---
+++ database/migrations/2022_06_01_023053_add_time_to__appointment_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddTimeToAppointmentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('appointments', function (Blueprint $table) {
+            $table->text('appointment_time')->after('appointment_date')->nullable();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('appointments', function (Blueprint $table) {
+            $table->dropColumn('appointment_time');
+
+        });
+    }
+}
